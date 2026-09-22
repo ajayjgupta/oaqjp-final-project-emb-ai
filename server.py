9c5e361dc793479ab6d87b7d4e9566b1e9db28fe
Ajay Gupta <ajayjgupta@gmail.com>
2026-09-22 19:31:44 +0530

    Phase 7 Changes

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 from EmotionDetection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route("/")
 def render_index_page():
-    return render_template("index.html")
+    return render_template('index.html')
 
 @app.route("/emotionDetector")
-def emotion_detection():
-    text_to_analyze = request.args.get("textToAnalyze")
+def emotion_detector_route():
+    text_to_analyze = request.args.get('textToAnalyze')
 
     response = emotion_detector(text_to_analyze)
 
-    formatted_response = (
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
+    return (
         f"For the given statement, the system response is "
         f"'anger': {response['anger']}, "
         f"'disgust': {response['disgust']}, "
@@ -22,8 +25,6 @@ def emotion_detection():
         f"'sadness': {response['sadness']}. "
         f"The dominant emotion is {response['dominant_emotion']}."
     )
-
-    return formatted_response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
